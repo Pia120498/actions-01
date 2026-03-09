e0855007f57bee5b7423363655b419827be2899d
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-03-09 13:40:20 +0530

    moved script outside of workflows

--- .github/workflows/sample-script.sh
+++
@@ -1,6 +0,0 @@
-echo "Hi good morning!"" 
-sudo apt-get install cowsay -y
-cowsay -f dragon "I am dragon... Shirorororororo!!!!!" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
-ls -lrta
