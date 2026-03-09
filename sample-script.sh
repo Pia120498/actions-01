aed08a755010365ded15f927f6792dc3695f5fb8
Priya Malewadkar <malewadkarpriya@gmail.com>
2026-03-09 13:41:49 +0530

    adding script

---
+++ sample-script.sh
@@ -0,0 +1,6 @@
+echo "Hi good morning!"" 
+sudo apt-get install cowsay -y
+cowsay -f dragon "I am dragon... Shirorororororo!!!!!" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -lrta
